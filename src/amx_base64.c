fcded59e7317d29cdc8d3b46de2e2fbfdffcf1e2
Daniel Shuey <daniel.shuey@gmail.com>
2025-09-22 16:54:43 +1000

    More verbose types just in case, may change later

--- src/amx_base64.c
+++ src/amx_base64.c
@@ -1,13 +1,13 @@
 #include "amx_base64.h"
 
-amx_s amx_base64_encode(const char *src)
+amx_base64_encoded amx_base64_encode(const char *src)
 {
-	amx_s result;
+	amx_base64_encoded result;
 	return result;
 }
 
-amx_s amx_base64_decode(const char *src)
+amx_base64_decoded amx_base64_decode(const char *src)
 {
-	amx_s result;
+	amx_base64_decoded result;
 	return result;
 }
--- src/amx_base64.h
+++ src/amx_base64.h
@@ -2,11 +2,13 @@
 
 #include <stddef.h>
 
-typedef struct {
+struct amx_base64_string {
 	int len;
 	char data[];
+};
 
-} amx_s;
+typedef struct amx_base64_string amx_base64_encoded;
+typedef struct amx_base64_string amx_base64_decoded;
 
-amx_s amx_base64_encode(const char *src);
-amx_s amx_base64_decode(const char *src);
+amx_base64_encoded amx_base64_encode(const char *src);
+amx_base64_decoded amx_base64_decode(const char *src);
