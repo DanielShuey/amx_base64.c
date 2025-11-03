b2e82fe96cbd98749c663989372d1dc4d8bd942e
Daniel Shuey <daniel.shuey@gmail.com>
2025-11-03 12:23:41 +1100

    Free results, file reads

--- tests/test_enc.c
+++ tests/test_enc.c
@@ -4,28 +4,35 @@
 
 char *b64enc(const char *s)
 {
-	int len   = strlen(s);
+	int   len = strlen(s);
 	char *buf = amx_base64_alloc(len);
 	amx_base64_encode(s, len, buf);
+
 	return buf;
 }
 
 void test_go_by_example()
 {
 	// from https://gobyexample.com/base64-encoding
-	const char *src = "abc123!?$*&()'-=@~";
-	const char *enc = "YWJjMTIzIT8kKiYoKSctPUB+";
+	const char *src    = "abc123!?$*&()'-=@~";
+	const char *enc    = "YWJjMTIzIT8kKiYoKSctPUB+";
+	char       *result = b64enc(src);
 
-	assert(strncmp(b64enc(src), enc, strlen(enc)) == 0);
+	assert(strncmp(result, enc, strlen(enc)) == 0);
+
+	free(result);
 }
 
 void test_tutorials_point()
 {
 	// from https://www.tutorialspoint.com/java8/java8_base64.htm
-	const char *src = "TutorialsPoint?java8";
-	const char *enc = "VHV0b3JpYWxzUG9pbnQ/amF2YTg=";
+	const char *src    = "TutorialsPoint?java8";
+	const char *enc    = "VHV0b3JpYWxzUG9pbnQ/amF2YTg=";
+	char       *result = b64enc(src);
 
-	assert(strncmp(b64enc(src), enc, strlen(enc)) == 0);
+	assert(strncmp(result, enc, strlen(enc)) == 0);
+
+	free(result);
 }
 
 void test_wikipedia()
@@ -44,18 +51,24 @@ dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu\
 dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo\
 ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
 
-	assert(strncmp(b64enc(src), enc, strlen(enc)) == 0);
+	char *result = b64enc(src);
+
+	assert(strncmp(result, enc, strlen(enc)) == 0);
+
+	free(result);
 }
 
 void test_ipsum()
 {
-	file fsrc       = readfile("fixtures/ipsum_4096.txt");
-	const char *src = fsrc.data;
+	file  fsrc   = readfile("fixtures/ipsum_4096.txt");
+	file  fenc   = readfile("fixtures/ipsum_4096_enc.txt");
+	char *result = b64enc(fsrc.data);
 
-	file fenc       = readfile("fixtures/ipsum_4096_enc.txt");
-	const char *enc = fenc.data;
+	assert(strncmp(result, fenc.data, strlen(fenc.data) - 1) == 0);
 
-	assert(strncmp(b64enc(src), enc, strlen(enc) - 1) == 0);
+	free(result);
+	free(fsrc.data);
+	free(fenc.data);
 }
 
 int main()
